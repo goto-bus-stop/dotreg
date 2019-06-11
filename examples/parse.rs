ef7d6dd2e89f1b214c4462e70f6d35b29d459469
Renée Kooi <renee@kooi.me>
2019-06-11 11:08:50 +0200

    Add micro cli

---
+++ examples/parse.rs
@@ -0,0 +1,13 @@
+use dotreg::read;
+
+fn main() {
+    match std::env::args().nth(1) {
+        Some(path) => {
+            let f = std::fs::File::open(path).unwrap();
+            println!("{:?}", read(f).unwrap());
+        },
+        None => {
+            eprintln!("usage: cargo run --example parse <filename>");
+        }
+    }
+}
